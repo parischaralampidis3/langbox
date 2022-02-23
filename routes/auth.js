4ad72ab4182dde353168ad9f5e5d767dde770fd3
Paris Charalampidis <curlycode@outlook.com>
2022-02-23 19:17:17 +0200

    set login route for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,17 @@
 const express = require( "express" );
 const router = express.Router();
 
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/default.json")
+const { body, validationResult } = require('express-validator');
+
+
+//bring schema 
+
+const User = require( "../models/User" );
+
+
  //@route  GET api/users
  //@description GET logged in user
  //@access  Private
@@ -17,8 +28,25 @@ router.get("/",( req,res ) =>{
  //@description authenticate the user & get token
  //@access  Public
 
-router.post("/", ( req,res ) =>{
+router.post("/", [
+    body("email", 'Please include a valid email').isEmail(),
+    body("password", "Password is required").exists()
+],
+
+async ( req,res ) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    //destructure user
+    const {email, password} = req.body;
+
     try{
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({msg:"Invalid credentials"});
+        }
+        
         res.send( "log in user" )
     }catch(err){
         res.status(500).json( "server error" )
