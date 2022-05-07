713f3f2ee386126a74e5f9eea0fbe80d0e88c472
Paris Charalampidis <curlycode@outlook.com>
2022-05-07 19:34:53 +0300

    creating frontend video call component

---
+++ frontend/my-client/src/Components/Socket/VideoCall.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const VideoCall = () => {
+  return (
+    <div>
+        <div className='text-center text-3xl font-bold items-center mt-12'>
+        <h1>VideoCall</h1>
+        </div>
+    </div>
+  )
+}
+
+export default VideoCall;
--- frontend/my-client/src/Pages/Dashboard.js
+++ frontend/my-client/src/Pages/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import AuthContext from '../Context/auth/authContext';
 import Greeting from '../Components/Greeting';
 import Logo from "../Components/designComponents/Logo";
-
+import VideoCall from '../Components/Socket/VideoCall';
 const Home = () => {
 
     const authContext = useContext(AuthContext)
@@ -13,16 +13,28 @@ const Home = () => {
     return (
 
         <div className="container mx-auto">
-            
+
+            <div className='flex w-full h-screen'>
             <div>
-                <Greeting />
+
+                <div className='mt-6  bg-gray-100 drop-shadow-lg '>
+                    <Greeting />
+                </div>
+
+                <div className='mt-6  bg-gray-100 drop-shadow-lg h-3/5 w-5/5'>
+                    <Logo />
+  
+                </div>
+
             </div>
 
-            <div className='mt-6 bg-gray-100 drop-shadow-lg w-1/3 h-screen'>
-                <Logo />
 
+
+            <div className=' mt-6 bg-gray-100 mx-auto items-center w-3/5 h-3/5 drop-shadow-lg'>
+              <VideoCall />
             </div>
 
+        </div>
         </div>
     )
 }
