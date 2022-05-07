d2955b55cb63af6ee691d1b7e41019ef6bce9dd6
Paris Charalampidis <curlycode@outlook.com>
2022-05-07 19:58:49 +0300

    subcomponents added

--- frontend/my-client/src/Components/Socket/VideoCall.js
+++ frontend/my-client/src/Components/Socket/VideoCall.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import Options from './Options';
+import Notifications from './Notifications';
+import VideoPlayer from './VideoPlayer';
 export const VideoCall = () => {
   return (
     <div>
         <div className='text-center text-3xl font-bold items-center mt-12'>
         <h1>VideoCall</h1>
         </div>
+
+
+        <VideoPlayer/>
+        <Options/>
+        <Notifications/>
+
     </div>
   )
 }
---
+++ frontend/my-client/src/Components/Socket/VideoPlayer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const VideoPlayer = () => {
+  return (
+    <div>VideoPlayer</div>
+  )
+}
+
+
+export default VideoPlayer;
